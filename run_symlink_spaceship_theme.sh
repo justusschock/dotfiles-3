532ec55af327a819a1c60d17263efe045a7df1c5
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-09-30 20:31:25 +0200

    Delete run_symlink_spaceship_theme.sh

--- run_symlink_spaceship_theme.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-ln -sfF ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
-ln -sfF ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/themes/spaceship.zsh-theme
