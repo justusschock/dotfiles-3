1aeff2fa43cb1c6d358bf3d4afe965bc73c1c0b1
Justus Schock <justus.schock@posteo.de>
2022-11-22 12:53:48 +0100

    Add dot_config/private_fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
