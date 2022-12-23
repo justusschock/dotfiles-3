92aea921c8c85e93a5a394527d2ca1deb1fd4675
Justus Schock <justus.schock@posteo.de>
2022-12-23 12:22:49 +0100

    Add .config/nvim_old/LICENSE Add .config/nvim_old/colors/default_theme.lua Add .config/nvim_old/.luacheckrc Add .config/nvim_old/.stylua.toml Add .config/nvim_old/init.lua Add .config/nvim_old/lua/configs/Comment.lua Add .config/nvim_old/lua/configs/aerial.lua Add .config/nvim_old/lua/configs/alpha.lua Add .config/nvim_old/lua/configs/autopairs.lua Add .config/nvim_old/lua/configs/better_escape.lua Add .config/nvim_old/lua/configs/bufferline.lua Add .config/nvim_old/lua/configs/cinnamon.lua Add .config/nvim_old/lua/configs/cmp.lua Add .config/nvim_old/lua/configs/colorizer.lua Add .config/nvim_old/lua/configs/feline.lua Add .config/nvim_old/lua/configs/gitsigns.lua Add .config/nvim_old/lua/configs/icons.lua Add .config/nvim_old/lua/configs/indent-line.lua Add .config/nvim_old/lua/configs/indent-o-matic.lua Add .config/nvim_old/lua/configs/lsp/handlers.lua Add .config/nvim_old/lua/configs/lsp/init.lua Add .config/nvim_old/lua/configs/lsp/server-settings/html.lua Add .config/nvim_old/lua/configs/lsp/server-settings/jsonls.lua Add .config/nvim_old/lua/configs/lsp/server-settings/ltex.lua Add .config/nvim_old/lua/configs/lsp/server-settings/pyright.lua Add .config/nvim_old/lua/configs/lsp/server-settings/rust_analyzer.lua Add .config/nvim_old/lua/configs/lsp/server-settings/sumneko_lua.lua Add .config/nvim_old/lua/configs/lsp/server-settings/texlab.lua Add .config/nvim_old/lua/configs/lsp/server-settings/tsserver.lua Add .config/nvim_old/lua/configs/luasnip.lua Add .config/nvim_old/lua/configs/neo-tree.lua Add .config/nvim_old/lua/configs/notify.lua Add .config/nvim_old/lua/configs/null-ls.lua Add .config/nvim_old/lua/configs/nvim-lsp-installer.lua Add .config/nvim_old/lua/configs/session_manager.lua Add .config/nvim_old/lua/configs/smart-splits.lua Add .config/nvim_old/lua/configs/telescope.lua Add .config/nvim_old/lua/configs/toggleterm.lua Add .config/nvim_old/lua/configs/treesitter.lua Add .config/nvim_old/lua/configs/which-key-register.lua Add .config/nvim_old/lua/configs/which-key.lua Add .config/nvim_old/lua/core/autocmds.lua Add .config/nvim_old/lua/core/mappings.lua Add .config/nvim_old/lua/core/options.lua Add .config/nvim_old/lua/core/plugins.lua Add .config/nvim_old/lua/core/status.lua Add .config/nvim_old/lua/core/ui.lua Add .config/nvim_old/lua/core/utils/git.lua Add .config/nvim_old/lua/core/utils/init.lua Add .config/nvim_old/lua/core/utils/updater.lua Add .config/nvim_old/lua/default_theme/base.lua Add .config/nvim_old/lua/default_theme/colors.lua Add .config/nvim_old/lua/default_theme/init.lua Add .config/nvim_old/lua/default_theme/lsp.lua Add .config/nvim_old/lua/default_theme/plugins/aerial.lua Add .config/nvim_old/lua/default_theme/plugins/beacon.lua Add .config/nvim_old/lua/default_theme/plugins/bufferline.lua Add .config/nvim_old/lua/default_theme/plugins/dashboard.lua Add .config/nvim_old/lua/default_theme/plugins/gitsigns.lua Add .config/nvim_old/lua/default_theme/plugins/highlighturl.lua Add .config/nvim_old/lua/default_theme/plugins/hop.lua Add .config/nvim_old/lua/default_theme/plugins/indent_blankline.lua Add .config/nvim_old/lua/default_theme/plugins/lightspeed.lua Add .config/nvim_old/lua/default_theme/plugins/neo-tree.lua Add .config/nvim_old/lua/default_theme/plugins/notify.lua Add .config/nvim_old/lua/default_theme/plugins/nvim-tree.lua Add .config/nvim_old/lua/default_theme/plugins/nvim-web-devicons.lua Add .config/nvim_old/lua/default_theme/plugins/rainbow.lua Add .config/nvim_old/lua/default_theme/plugins/symbols_outline.lua Add .config/nvim_old/lua/default_theme/plugins/telescope.lua Add .config/nvim_old/lua/default_theme/plugins/vimwiki.lua Add .config/nvim_old/lua/default_theme/plugins/which-key.lua Add .config/nvim_old/lua/default_theme/treesitter.lua Add .config/nvim_old/lua/default_theme/utils.lua Add .config/nvim_old/packer_snapshot

---
+++ dot_config/nvim_old/lua/configs/lsp/server-settings/ltex.lua
@@ -0,0 +1,10 @@
+return {
+  settings = {
+    ltex = {
+      additionalRules = {
+      -- https://dev.languagetool.org/finding-errors-using-n-gram-data.html
+        languageModel = '~/ngrams'
+      }
+    }
+  }
+}
