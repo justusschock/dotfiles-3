a9484481d7cf64e07bd498a527ad76bf66d4c0c0
Justus Schock <justus.schock@posteo.de>
2023-08-28 14:22:49 +0200

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,11 +4,13 @@ end
 
 # lightning
 set -x LIGHTNING_SQUAD_PROJECT_ID "22AF95B8-D4CE-487A-9147-9465FC80E379"
+set -x NODE_OPTIONS "--openssl-legacy-provider"
+set -x AWS_DEFAULT_PROFILE "byoc-sandbox"
+set -x AWS_PROFILE "byoc-sandbox"
 
-# Go
 set -x GOPATH $HOME/go
-set -x PATH $GOPATH/bin:$PATH
-
+set -x GOROOT /opt/homebrew/Cellar/go/1.20.2/libexec
+set PATH /opt/homebrew/bin /opt/homebrew/opt/gnu-sed/libexec/gnubin $GOPATH/bin $GOROOT/bin $PATH
 # Unfortunately nvim is not available on most systems, so use vim as fallvack
 if type -q nvim;
   set -x EDITOR 'nvim';
@@ -19,6 +21,7 @@ end
 # load homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+
 # conda
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
@@ -28,6 +31,7 @@ eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv |
 # Work Related -> lightning.ai
 alias L="lightning"
 alias LP="LIGHTNING_CLOUD_PROJECT_ID=$LIGHTNING_SQUAD_PROJECT_ID lightning"
+alias make_develop="make develop && docker stop grid-backend-grid-controlplane-1"
 
 # exa > ls
 alias ls="exa --icons --group-directories-first"
